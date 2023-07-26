1fcccce53e5ee28fe1828bd329b1b04e2a65bb37
Renee Stanley <renee.breaks@gmail.com>
2023-07-26 03:07:39 +0000

    adds challenge 02

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  My First Bash Script
+# Author:                       Renee Stanley
+# Date of latest revision:      07/25/2023
+# Purpose:                      To learn how to use back prints and variables.
+
+# Declaration of variables
+
+
+yay="I am working perfectly!"
+
+# Main
+echo $yay
+
+
+# End
